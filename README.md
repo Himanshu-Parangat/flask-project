824fd7dd87448f2dae99269b5f7c37c14ec6c1e6
Himanshu-Parangat <martinmagnicoc@gmail.com>
2022-10-25 16:58:12 +0530

    error handling 404,500

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # my persional flask project
+
+
+
+get dependency
+```python
+python3 -r requirements.txt
+```
+
+run website using 
+```python
+python3 flaskrun.py
+```
--- flaskrun.py
+++ flaskrun.py
@@ -32,6 +32,18 @@ def user_dashboard(user_name):
    # return "welcome back user %s!" % user_name 
 
 
+#Invalid Url
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+# Internal server error
+@app.errorhandler(500)
+def special_exception_handler(error):
+    return render_template('500.html'), 500
+
+
 if (__name__ == "__main__" ):
     app.run(debug=True)
 
